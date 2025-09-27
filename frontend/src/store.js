01e4fe09b76908de290252a772d88f87237491b5
nasif-muhamed <muhdnasifk@gmail.com>
2025-09-27 14:38:45 +0530

    fix(frontend/redux): resolve Redux v5 peer dependency conflict

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@
 // store.js
 import {  createStore, applyMiddleware } from "redux"
 import {thunk} from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from '@redux-devtools/extension'
 import rootReducer from "./projectreducer/ProjectReducer"
 
 
